36f0970306927dc089838ae03fa3972b61e2c471
syra <syra@example.com>
2025-11-22 17:05:59 +0700

    update fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,14 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import React from 'react'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Web Teknologi',
-  description: 'Belajar Next.js dan Tailwind',
-}
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {children}
-      </body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  )
+  );
 }
